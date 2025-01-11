1c524c453c54b54c2e6d5b0517492f28e87a679d
yajing03 <yajing03@student.ubc.ca>
2025-01-10 22:22:35 -0800

    Adding test files

--- R/split1.R
+++ R/split1.R
@@ -9,6 +9,11 @@
 #' @examples
 #' x <- "alfa,bravo,charlie,delta"
 #' strsplit1(x, split = ",")
-strsplit1 <- function(x, split) {
-  strsplit(x, split = split)[[1]]
+str_split_one <- function(string, pattern, n = Inf) {
+  stopifnot(is.character(string), length(string) <= 1)
+  if (length(string) == 1) {
+    stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
+  } else {
+    character()
+  }
 }
